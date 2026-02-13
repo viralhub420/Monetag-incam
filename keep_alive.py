7cba059a70e70cab74f4b16cb15adeeaa0734d78
viralhub420 <hshamimbd355@gmail.com>
2026-02-13 15:04:42 +0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,14 @@
+from flask import Flask
+import threading
 
+def run_web():
+    app = Flask("")
+
+    @app.route("/")
+    def home():
+        return "Bot is Running"
+
+    threading.Thread(
+        target=lambda: app.run(host="0.0.0.0", port=10000),
+        daemon=True,
+    ).start()
