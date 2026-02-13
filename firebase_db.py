e30f47d2f23214dc65b160cf72c01f1fdf6ad444
viralhub420 <hshamimbd355@gmail.com>
2026-02-13 09:47:52 +0600

    Update firebase_db.py

--- firebase_db.py
+++ firebase_db.py
@@ -1,1 +1,20 @@
+import os
+import json
+import firebase_admin
+from firebase_admin import credentials, db
 
+def init_firebase():
+    if not firebase_admin._apps:
+        firebase_json = os.getenv("FIREBASE_CREDENTIALS")
+
+        if not firebase_json:
+            raise ValueError("FIREBASE_CREDENTIALS not found in environment variables")
+
+        cred_dict = json.loads(firebase_json)
+        cred = credentials.Certificate(cred_dict)
+
+        firebase_admin.initialize_app(cred, {
+            "databaseURL": os.getenv("DATABASE_URL")
+        })
+
+    return db.reference("/")
