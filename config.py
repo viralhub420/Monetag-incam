d21d1cb47dc6833e391938d905a962a034c05a1c
viralhub420 <hshamimbd355@gmail.com>
2026-02-13 10:36:45 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+import os
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+MAIN_CHANNEL = os.getenv("MAIN_CHANNEL")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN is missing in environment variables")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is missing in environment variables")
